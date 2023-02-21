2b85dd0d9417ea343f8785b64751386859b104dd
Sahil0336 <mohamedsahil0336@gmail.com>
2023-02-22 01:49:54 +0530

    Don't Know what to do

---
+++ Java Eclipse/eclipse-workspace/firstecli/src/firstecli/wikifirst.java
@@ -0,0 +1,12 @@
+package firstecli;
+
+public class wikifirst {
+
+	public static void main(String[] args) {
+		int a =21; int b = 30; int s= a+b;
+		System.out.println("First Java project ever...");
+		System.out.println("Sum of a and b is:"+s);
+
+	}
+
+}
